cae11e5ef9fcbf0bd8ace5465ec91207b2c02d5a
wunian <352262437@qq.com>
2022-03-18 17:00:23 +0800

    response data is object  analysis

--- src/utils/analysis.ts
+++ src/utils/analysis.ts
@@ -3,26 +3,32 @@
  * @param {Object} level 递归层级
  */
 export function parseObject(properties: any, level: number, required: Array<string>){
-	let requestStr = ''
-	requestStr += ''.padEnd((level-1) * 2, ' ')		
+	let requestStr = '', spaces = ''.padEnd((level-1) * 2, ' ')
+	// console.log(level, properties)
+	requestStr += spaces
 	requestStr += '{\n'
 	for(let d  in properties) {
 		let key1 = d as keyof typeof properties;
 		let value = properties[key1] as any;
 		requestStr += ''.padEnd(level * 2, ' ');
 		const isRequired = required ? required.find(v => v === key1.toString()) : false
-		console.log(required, key1.toString())
+		// console.log(required, key1.toString())
 		if(value?.type === 'array'){
-			requestStr += `${key1.toString()}: [\n`
+			requestStr += `${key1.toString()}: \n`
 			requestStr += parseObject(value?.items?.properties, level + 1, value?.items?.required)
-			requestStr += ''.padEnd(level * 2, ' ')
-			requestStr += `],\n`
+			// requestStr += ''.padEnd(level * 2, ' ')
+			requestStr += `[],\n`
+		} else if (value?.type === 'object') {
+			requestStr += `${key1.toString()}: \n`
+			requestStr += parseObject(value?.properties, level + 1, value?.required)
+			requestStr += `\n`
 		} else {
-			console.log(value)
+			// console.log(value)
 			requestStr += `${key1.toString()}${!isRequired?'?': ''}: ${value?.type},${value?.description ? ' \/\/ ' + value?.description : ''}\n`
 		}
 	}
-	requestStr += ''.padEnd((level-1) * 2, ' ')
-	requestStr += `}\n`
+	requestStr += spaces
+	requestStr += `}`
+	// console.log(level, properties)
 	return requestStr
 }
